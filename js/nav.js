f0cbcd55ec5fce59058c7bada17245303dca2751
황유진 <carrot070410@naver.com>
2024-11-28 14:51:31 +0900

    nav 작동 확인

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,17 @@
 function toggleMenu(){
-
     const navToggleDiv = document.getElementsByClassName("nav-toggle")[0];
+    console.log(navToggleDiv);
     // const navTogleDiv = document.getElementsByIdName("nav-toggle");
     const navListUl = document.getElementsByClassName("nav-list")[0];
     const  toggleI = navToggleDiv.getElementsByTagName("i")[0];
     
-        navToggleDiv.onclick = (event) => {
-            navListUl.classList.toggle("show-menu");
-
-            toggleI.classList.toggle("bi-list");
-            toggleI.classList.toggle("bi-x-lg");
-
-        
+    navToggleDiv.onclick = (event) => {
+        navListUl.classList.toggle("show-menu");
+        toggleI.classList.toggle("bi-list");
+        toggleI.classList.toggle("bi-x-lg");
     
-        }
+
     }
-    toggleMenu();
+}
+toggleMenu();
+console.log("dltkdgo");
